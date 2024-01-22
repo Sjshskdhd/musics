f7b139e1f41d094144c68da5aa25678f4742137c
ELGAZAR_ALWAZER❤ <108340300+Sjshskdhd@users.noreply.github.com>
2024-01-22 16:42:10 +0200

    Update ترحيب.py

--- SedthonMusic/plugins/sedthon/ترحيب.py
+++ SedthonMusic/plugins/sedthon/ترحيب.py
@@ -1,27 +1,20 @@
-import asyncio
+import os
 from pyrogram import Client, filters
-from datetime import datetime
+from pyrogram.types import Message, User
+from pyrogram import Client, emoji 
+from SedthonMusic import app
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, ChatPermissions
+from SedthonMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from pyrogram import filters
 
-@app.on_message(filters.member_joined)
-async def get_chat_info(chat, already=False):
-    if not already:
-        chat = await app.get_chat(chat)
-    chat_id = chat.id
-    members = chat.members_count
-	await message.reply_text(f"""
-● نورت يقمر ♥♡
-{message.from_user.first_name}
-●  يجب احترام الادمنية
-●  الالتزام بالقوانين في الوصف
-●  الاعضاء  {members} 
-""")
-
-
-@app.on_message(filters.member_left)
-async def leftmem(chat):
-    await message.reply_text(f"""
-    ★انت مش جدع يا «{ message.from_user.first_name}» 
-★ ✞ حد يكون فى بار جامد زى ده ويخرج ❤️‍🔥🌚
-✞ ده حتى كلنا اخوات يجدع ❤️‍🔥🌚
-✞يلا في دهيه 🌚❤️‍🔥
-√""")
+@app.on_message(filters.new_chat_members)
+async def wel__come(client: Client, message: Message):
+	chatid= message.chat.id
+	await client.send_message(text=f"- نورت ياا فرتكهه😘🤝️ {message.from_user.mention}\n│ \n└ʙʏ في {message.chat.title}",chat_id=chatid)
+	
+@app.on_message(filters.left_chat_member)
+async def good_bye(client: Client, message: Message):
+	chatid= message.chat.id
+	await client.send_message(text=f"- مشيت ليه يوحش يلا بسلامات🥲👋\n│ \n└ʙʏ  {message.from_user.mention} ",chat_id=chatid)
+	
